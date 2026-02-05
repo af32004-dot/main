bc425e072fb806e69311f1edd2462f5cb87fd544
af32004-dot <af32004@gmail.com>
2026-02-05 14:12:24 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import React from "react";
-
-import App from './App'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(  <App  />)
