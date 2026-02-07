17fede695a50661104f53c488881c66258e2ef55
af32004-dot <af32004@gmail.com>
2026-02-07 17:15:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*require('dotenv').config()*/
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const Note = require('./models/nte')
@@ -10,76 +10,16 @@ const cors = require('cors')
 
 app.use(cors())
 
+
 app.use(express.static('dist'))
+
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
+
 app.use(express.json())
 app.use(morgan(':method :url :body'))
-
-
-/*
-//mangoooseeee
-const mongoose = require('mongoose')
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const password = process.argv[2]
-const url = `mongodb+srv://af32004_db_user:${password}@cluster0.0re4fso.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  name: String,
-  num: String,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-    delete returnedObject.id
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-//mangoooseeee
-*/
-
-/*
-const note = new Note({
-  name: nm,
-  num: nb,
-})
-*/
-
-/*
-let notes = [
-   { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "num": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "num": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "num": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "num": "39-23-6423122"
-    }
-]
-
-*/
 
 const requestLogger = (request, response, next) => {
  
@@ -91,15 +31,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
-
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -135,41 +66,6 @@ console.log ('find',body.name,'find2', Note.find({}).then(notes => {    response
   })
 })
 
-/*
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }
-
-  if (!body.num) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
-  }
-
-
-  if (notes.filter(np=>np.name===body.name).length!==0) {
-    return response.status(400).json({ 
-      error: 'nome già presente' 
-    })
-  }
-
-
-  const persona = {
-    name: body.name,
-    num: body.num || '00',
-    id: generateId(),
-  }
-
-  notes= notes.concat(persona)
-
-  response.json(persona)
-})
-*/
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -185,61 +81,44 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-/*
-app.get('/notes', (request, response) => {
-  response.json(notes)
-})
-*/
-
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
+	if(note) {
     response.json(note)
+        } else {
+  	response.status(404).end()
+        }
   })
+.catch(error => next(error))
+   
 })
 
-/*
-app.get('/notes/:id', (request, response) => {
-  const id = request.params.id
-  const persona = notes.find(nt => nt.id === id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { name, num } = request.body
 
- if (persona) {
-    response.json(persona)
-  } else {
-    response.status(404).end()
-  }
-})
-*/
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
 
-app.put('/notes/:id', (request, response) => {
-  const id = request.params.id
-  const body = request.body
+      note.name = name
+      note.num = num
 
-  if (!body.num) {
-    return response.status(400).json({ 
-      error: 'number missing' 
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
     })
-  }
-
-  
-  const persona = {
-    name: body.name,
-    num: body.num || '00',
-    id: id
-  }
-
-  notes[id-1]= persona
-
-  response.json(persona)
+    .catch(error => next(error))
 })
 
-
-app.delete('/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(nt => nt.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -251,5 +130,17 @@ app.use(unknownEndpoint)
 const PORT = process.env.PORT /* || 3001 */
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-
 })
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
