ce2030d552ae30ba4451cff14e1b9c7c5f989754
af32004-dot <af32004@gmail.com>
2026-02-07 16:28:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Note = require('./models/nte')
+
 const app = express()
 app.use(morgan('tiny'))
-/*
+
 const cors = require('cors')
 
 app.use(cors())
-*/
-app.use(express.static('dist'))
 
+app.use(express.static('dist'))
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -17,8 +19,43 @@ app.use(express.json())
 app.use(morgan(':method :url :body'))
 
 
-app.use(express.json())
+/*
+//mangoooseeee
+const mongoose = require('mongoose')
 
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const password = process.argv[2]
+const url = `mongodb+srv://af32004_db_user:${password}@cluster0.0re4fso.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  num: String,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.id
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+//mangoooseeee
+*/
+
+/*
+const note = new Note({
+  name: nm,
+  num: nb,
+})
+*/
+
+/*
 let notes = [
    { 
       "id": "1",
@@ -42,7 +79,7 @@ let notes = [
     }
 ]
 
-
+*/
 
 const requestLogger = (request, response, next) => {
  
@@ -64,10 +101,7 @@ const generateId = () => {
 }
 
 
-
-
-
-app.post('/notes', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.name) {
@@ -82,11 +116,48 @@ app.post('/notes', (request, response) => {
     })
   }
 
+/*
+console.log ('find',body.name,'find2', Note.find({}).then(notes => {    response.json(notes)  }))
+  if (Note.find(body.name).length!==0) {
+    return response.status(400).json({ 
+      error: 'nome già presente' 
+    })
+  }
+*/
+
+  const note = new Note({
+    name: body.name,
+    num: body.num || '0',
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+/*
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.num) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+
   if (notes.filter(np=>np.name===body.name).length!==0) {
     return response.status(400).json({ 
       error: 'nome già presente' 
     })
   }
+
 
   const persona = {
     name: body.name,
@@ -98,7 +169,7 @@ app.post('/notes', (request, response) => {
 
   response.json(persona)
 })
-
+*/
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -108,10 +179,25 @@ app.get('/info', (request, response) => {
   response.send('<p>ci sono '+ notes.length + ' persone</p><p>' + Date() + '</p>')
 })
 
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+/*
 app.get('/notes', (request, response) => {
   response.json(notes)
 })
+*/
 
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
+
+/*
 app.get('/notes/:id', (request, response) => {
   const id = request.params.id
   const persona = notes.find(nt => nt.id === id)
@@ -122,14 +208,37 @@ app.get('/notes/:id', (request, response) => {
     response.status(404).end()
   }
 })
+*/
 
-app.delete('/api/notes/:id', (request, response) => {
+app.put('/notes/:id', (request, response) => {
+  const id = request.params.id
+  const body = request.body
+
+  if (!body.num) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  
+  const persona = {
+    name: body.name,
+    num: body.num || '00',
+    id: id
+  }
+
+  notes[id-1]= persona
+
+  response.json(persona)
+})
+
+
+app.delete('/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter(nt => nt.id !== id)
 
   response.status(204).end()
 })
-
 
 
 const unknownEndpoint = (request, response) => {
@@ -139,7 +248,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001 /**/
+const PORT = process.env.PORT /* || 3001 */
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
