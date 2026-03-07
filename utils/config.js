807d1945d0a122ad74c7ed09e27336c2b3696726
af32004-dot <af32004@gmail.com>
2026-03-07 09:15:35 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -2,11 +2,12 @@
 //require('dotenv').config()
 
 const PORT = process.env.PORT  || 3001
-/* const MONGODB_URI = process.env.NODE_ENV === 'test' 
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
-const SECRET = process.env.SECRET */
+const SECRET = process.env.SECRET /**/
 
 module.exports = { /*MONGODB_URI, SECRET,*/ PORT }
+
 
 
